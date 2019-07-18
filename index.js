cb0e7bac5448f0fc931ffcea5da9afcd83e584d0
Nastassie <50500307+Nastassie@users.noreply.github.com>
2019-07-18 23:22:46 +0300

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
+const rootElement = document.getElementById('root');
+const loadingElement = document.getElementById('loading-overlay');
+rootElement.innerText = 'Loading...';
+ fetch(API_URL)
+    .then(response => {
+         if (!response.ok) {
+          throw new Error('Failed load data');
+         }
+         return response.json();
+     })
+    .then(file => {
+         const fighters = JSON.parse(atob(file.content));
+         const names = fighters.map(it => it.name).join('\n');
 
+         rootElement.innerText = names;
+        // loadingElement.remove();
+    })
+    .catch(error => {
+        console.warn(error);
+        root.innerText = 'Failed to load data';
+
+    })
+     .finally(() => {
+         loadingElement.remove(); 
+     });
+     
+
+  
